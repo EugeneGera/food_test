7cc60849a5fa4b15e5c90ac580526709f8bad3f5
Eugene <example@gmail.com>
2022-08-02 22:34:56 +0300

    :commit webpack

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -152,4 +152,4 @@ function slider() {
         return +str.replace(/\D/g, '');
     }
 }
-module.exports = slider;
+export default slider;
--- js/modules/timer.js
+++ js/modules/timer.js
@@ -59,4 +59,4 @@ function timer() {
     setClock('.timer', deadline);
 }
 
-module.exports = timer;
+export default timer;
